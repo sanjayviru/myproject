a1bd39f33fedac88cbdd4a57de7e522468ac5a4d
sanjayviru <145086488+sanjayviru@users.noreply.github.com>
2023-12-04 12:10:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
   res.end('Hello, Node.js!\n');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
